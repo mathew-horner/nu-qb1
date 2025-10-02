5bce0ce87417ddc8659b08e4974b2507ab575a36
Mathew Horner <mathewhorner456@gmail.com>
2025-10-02 14:26:21 -0500

    Use better weighting method

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ import pickle
 import sys
 import warnings
 
+from datetime import datetime as dt
 from sklearn.linear_model import LinearRegression
 from sklearn.multioutput import MultiOutputRegressor
 
@@ -19,9 +20,21 @@ warnings.filterwarnings("ignore")
 def train(args):
     data = pd.read_csv("data/stats.csv")
 
+    # Parse date strings into date objects in DataFrame.
+    data["date"] = data["date"].map(lambda date: dt.strptime(date, "%Y-%m-%d"))
+
+    min_date = min(data["date"])
+    max_date_spread = (dt.now() - min_date).days
+
+    def calculate_weight(game_date, max_date_spread):
+        """Calculate weight to assign to game sample based on how long ago it was played."""
+        date_spread = (dt.now() - game_date).days
+        return max(0.1, 1.0 - (date_spread / max_date_spread))
+
+    weights = [calculate_weight(date, max_date_spread) for date in data["date"]]
+
     X = data[["opp_defense_skill"]]
     y = data[["comp_pct", "yds", "tds", "ints"]]
-    weights = data[["weight"]].values.flatten()
 
     model = MultiOutputRegressor(LinearRegression())
     model.fit(X, y, sample_weight=weights)
