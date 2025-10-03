dac50315b558488432eb7e950cebb37cc0d33576
Mathew Horner <mathewhorner456@gmail.com>
2025-10-02 21:55:54 -0500

    Add ability to calculate mean absolute percentage error

--- model.py
+++ model.py
@@ -9,16 +9,16 @@ from datetime import datetime as dt
 from sklearn.linear_model import LinearRegression
 from sklearn.multioutput import MultiOutputRegressor
 
-# File name to pickle the model data to
+# File name to pickle the model data to.
 MODEL_PICKLE_FILE = "model.pkl"
 
-# File name to read stats from
+# File name to read stats from.
 STATS_FILE = "data/stats.csv"
 
 warnings.filterwarnings("ignore")
 
 def train(args):
-    data = pd.read_csv("data/stats.csv")
+    data = pd.read_csv(STATS_FILE)
 
     # Parse date strings into date objects in DataFrame.
     data["date"] = data["date"].map(lambda date: dt.strptime(date, "%Y-%m-%d"))
@@ -43,10 +43,9 @@ def train(args):
         pickle.dump(model, f)
 
 def predict(args):
-    with open(MODEL_PICKLE_FILE, "rb") as f:
-        model = pickle.load(f)
-
     opp_defense_skill = float(args.rating)
+
+    model = load_model()
     pred = model.predict([[opp_defense_skill]])
 
     comp_pct, yds, tds, ints = pred[0]
@@ -64,6 +63,34 @@ def predict(args):
         print("Completion %,Yards,TDs,INTs")
         print(f"{comp_pct},{yds},{tds},{ints}")
 
+def evaluate(args):
+    # The fields to evaluate MAPE on. We don't do this on TDs and INTs because
+    # of their propensity towards values of 0, which MAPE does not work well on.
+    FIELDS = ["comp_pct", "yds"]
+
+    data = pd.read_csv(STATS_FILE)
+    model = load_model()
+    sigmas = [0 for _ in range(len(FIELDS))]
+
+    for row_index, row in data.iterrows():
+        rating = row["opp_defense_skill"]
+        pred = model.predict([[rating]])[0]
+        actual = list(row[FIELDS])
+        for idx in range(len(FIELDS)):
+            sigmas[idx] += abs((actual[idx] - pred[idx]) / actual[idx])
+
+    n = row_index + 1
+    for idx in range(len(FIELDS)):
+        sigmas[idx] = 100 * (1 / n) * sigmas[idx]
+
+    print("MAPEs")
+    for idx, field in enumerate(FIELDS):
+        print(f"{field}: {round(sigmas[idx], 2)}%")
+
+def load_model():
+    with open(MODEL_PICKLE_FILE, "rb") as f:
+        return pickle.load(f)
+
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
 
@@ -74,6 +101,9 @@ predict_parser = subparsers.add_parser("predict")
 predict_parser.add_argument("rating")
 predict_parser.add_argument("-f", "--format", choices=["readable", "csv"], default="readable")
 predict_parser.set_defaults(func=predict)
+
+evaluate_parser = subparsers.add_parser("evaluate")
+evaluate_parser.set_defaults(func=evaluate)
 
 args = parser.parse_args()
 args.func(args)
